170a43b61e939c3c87b7a7d270e780823ce28798
Rupert Foggo McKay <maxfmckay@gmail.com>
2025-03-27 16:04:35 +0100

    now with speed display

--- src/index.ts
+++ src/index.ts
@@ -14,16 +14,26 @@ if (!video) {
 const minusButton = document.createElement("button");
 minusButton.append("-");
 minusButton.classList.add("playerButton", "ytp-button");
+minusButton.style.fontSize = "50px";
 minusButton.addEventListener("click", () => {
   video.playbackRate -= 0.01;
+  speedDisplay.textContent = `${video.playbackRate * 100}%`;
 });
+
+// Initialize speed display
+const speedDisplay = document.createElement("span");
+speedDisplay.append("100%");
+speedDisplay.classList.add("playerButton", "ytp-button");
+speedDisplay.style.fontSize = "50px";
 
 // Initialize plus button
 const plusButton = document.createElement("button");
 plusButton.append("+");
 plusButton.classList.add("playerButton", "ytp-button");
+plusButton.style.fontSize = "50px";
 plusButton.addEventListener("click", () => {
   video.playbackRate += 0.01;
+  speedDisplay.textContent = `${video.playbackRate * 100}%`;
 });
 
 // Insert the controls into the UI
